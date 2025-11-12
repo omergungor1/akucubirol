c88d1bc1715bdb115ec8a1c733ae64a24de3856b
Ömer <omergungorco@gmail.com>
2025-11-12 10:54:51 +0300

    tagmanager added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTSK7PLZ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Preconnect Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -20,6 +32,17 @@ export default function Document() {
         <meta name="theme-color" content="#0D1B2A" />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KTSK7PLZ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Skip to content link - Erişilebilirlik */}
         <a href="#main-content" className="skip-link">
           Ana içeriğe geç
